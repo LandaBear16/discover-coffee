728a6c3948730a7c6163b5e3657ac9572b250221
Yolanda Collins <yolanda.collins@gwtinsight.com>
2024-03-12 10:42:55 +0000

    adding client banner file

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Banner from "@/components/banner.client";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Coffee Connoisseur</h1>
+      <Banner />
     </main>
   );
 }
---
+++ components/banner.client.tsx
@@ -0,0 +1,24 @@
+'use client'
+import React from 'react'
+
+const Banner = () => {
+  const handleOnClick = () => {}
+  return (
+    <div className="mb-12 grid lg:mb-24 lg:grid-cols-2">
+      <div className="z-20 flex flex-col px-2 md:pt-12">
+        <h1 className="my-2 flex-wrap">
+          <span className="pr-2 text-white">Coffee</span>
+          <span className="text-gray-900">Connoisseur</span>
+        </h1>
+        <p className="font-sans text-xl font-semi-bold text-gray-900 md:mt-5 lg:text-2xl">
+          Discover your local coffee shops!
+        </p>
+        <button className="mt-12" onClick={handleOnClick}>
+          View Stores Nearby
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
